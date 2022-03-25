216c802d5609b11a2b65c80e273aaff72446d915
therwal <waltherkg@gmail.com>
2022-03-25 19:16:37 +0100

    download and imageSize selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react';
+import { toPng } from 'html-to-image';
+import download from 'downloadjs';
 
 import ImageWindow from './components/ImageWindow';
 import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
+  const [imageSize, setImageSize] = useState(1024);
+
+  const changeImageSize = (e) => {
+    setImageSize(e);
+  };
+
+  const downloadImage = () => {
+    toPng(document.querySelector('.ImageWindow'), {
+      canvasWidth: imageSize,
+      canvasHeight: imageSize,
+    }).then((dataUrl) => {
+      download(dataUrl, 'image.png');
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +28,11 @@ function App() {
       </header>
       <main>
         <ImageWindow />
-        <Sidebar />
+        <Sidebar
+          imageSize={imageSize}
+          changeImageSize={changeImageSize}
+          downloadImage={downloadImage}
+        />
       </main>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
 import './Sidebar.css';
 
-function Sidebar() {
+function Sidebar({ imageSize, changeImageSize, downloadImage }) {
+  const handleChange = (e) => {
+    changeImageSize(e.target.value);
+  };
   return (
     <div className="Sidebar">
-      <select value="">
+      <select value={imageSize} onChange={handleChange}>
         <option value="512">Small 512x512</option>
         <option value="1024">Medium 1024x1024</option>
         <option value="2048">Large 2048x2048</option>
       </select>
-      <button>Download</button>
+      <button onClick={downloadImage}>Download</button>
     </div>
   );
 }
