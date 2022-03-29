13e12cb83ab615758209b60e6ac03b9b27e143a3
therwal <waltherkg@gmail.com>
2022-03-29 21:45:21 +0200

    change images from public to cloud

--- src/components/WhoVoted.jsx
+++ src/components/WhoVoted.jsx
@@ -2,7 +2,11 @@ import './WhoVoted.css';
 
 const WhoVoted = ({ pro, partylist }) => {
   const parties = partylist.map((p) => (
-    <img src={`${p.name}.png`} alt={p.name} />
+    // <img src={`${p.name}.png`} alt={p.name} />
+    <img
+      src={`https://res.cloudinary.com/daq5ahswm/image/upload/v1648383574/party%20logos/${p.name}.png`}
+      alt={p.name}
+    />
   ));
   return (
     <div className={`WhoVoted ${pro ? 'WhoVoted-green' : 'WhoVoted-red'}`}>
